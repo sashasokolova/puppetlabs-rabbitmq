6557db171845e0a267cd03bea6a903d911f149bd
Alberto Rodriguez Ortega <aortega@afilias.info>
2015-12-11 15:02:56 +0000

    Typo

--- manifests/config.pp
+++ manifests/config.pp
@@ -88,7 +88,7 @@ class rabbitmq::config {
     mode   => '0644',
   }
 
-  file { '${etc_dir}/rabbitmq/ssl':
+  file { "${etc_dir}/rabbitmq/ssl":
     ensure => directory,
     owner  => '0',
     group  => '0',
